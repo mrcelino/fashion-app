b129ab2b5403c567af0acd85ef6b92fdb676cf94
Marcelino <marcelinokenny12@gmail.com>
2025-07-10 22:47:00 +0700

    add layout

---
+++ src/app/(routes)/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from "../../components/Navbar";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <Sidebar />
+      <main className="pt-20 md:pt-24 min-h-screen ml-80 px-4 md:px-8">
+        {children}
+      </main>
+    </div>
+  );
+}
