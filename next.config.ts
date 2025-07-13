c515a03aeae7e9b361a91215da3b7969d8c9ec16
Marcelino <marcelinokenny12@gmail.com>
2025-07-13 23:11:47 +0700

    feat: configure remote image patterns for improved asset handling

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'rxfhqcwirdrtwprerjjs.supabase.co', // Hostname dari error
+        port: '',
+        pathname: '/storage/v1/object/public/items/images/**', // Path opsional untuk lebih spesifik
+      },
+    ],
+  },
 };
 
 export default nextConfig;
