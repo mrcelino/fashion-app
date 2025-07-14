942512258dd91885ea95060e9ab79959958d8c98
Marcelino <marcelinokenny12@gmail.com>
2025-07-14 21:24:23 +0700

    feat: add Firebase SDK and initialize Firebase client

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,25 @@
 import type { NextConfig } from "next";
-
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'rxfhqcwirdrtwprerjjs.supabase.co', // Hostname dari error
+        hostname: 'rxfhqcwirdrtwprerjjs.supabase.co',
         port: '',
-        pathname: '/storage/v1/object/public/items/images/**', // Path opsional untuk lebih spesifik
+        pathname: '/storage/v1/object/public/items/images/**',
       },
     ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: isProd
+          ? ""
+          : "http://localhost:8080/api/v1/:path*",
+      },
+    ];
   },
 };
 
---
+++ src/app/lib/firebaseClient.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDpMtG1gffo0hK63oecl_-Id0cJ_ZkDhNI",
+  authDomain: "satu-lemari-f2614.firebaseapp.com",
+  projectId: "satu-lemari-f2614",
+  storageBucket: "satu-lemari-f2614.firebasestorage.app",
+  messagingSenderId: "814764272898",
+  appId: "1:814764272898:web:03a4c2fe120c87e510661b",
+  measurementId: "G-YZWC6JE8CG"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
